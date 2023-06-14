85b1454e441a2814f5c76bd848986bb917e7fbb5
Charlie Carolan <charliecarolan1999@gmail.com>
2023-06-14 01:05:16 +0100

    feat(nvim): fix nvim-treesitter config

--- neovim/.config/nvim/lua/tldrqwerty/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/tldrqwerty/plugins/treesitter.lua
@@ -2,26 +2,56 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
+    event = { "BufReadPost", "BufNewFile" },
     opts = {
-      ensure_installed = "all",
-      sync_install = false,
-      auto_install = true,
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = { "markdown" },
+      highlight = { enable = true },
+      indent = { enable = true },
+      ensure_installed = {
+        "bash",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "luadoc",
+        "luap",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "vimdoc",
+        "yaml",
       },
-      context_commentstring = {
+      incremental_selection = {
         enable = true,
-        enable_autocmd = false,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = false,
+          node_decremental = "<bs>",
+        },
       },
-    }
-  },
-  {
-    "nvim-treesitter/nvim-treesitter-textobjects",
-    dependencies = { "nvim-treesitter" },
-  },
-  {
-    "nvim-treesitter/playground",
-    dependencies = { "nvim-treesitter" },
+    },
+    dependencies = {
+      { "nvim-treesitter/nvim-treesitter-textobjects" },
+      { "nvim-treesitter/playground" },
+    },
+    config = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        ---@type table<string, boolean>
+        local added = {}
+        opts.ensure_installed = vim.tbl_filter(function(lang)
+          if added[lang] then
+            return false
+          end
+          added[lang] = true
+          return true
+        end, opts.ensure_installed)
+      end
+      require("nvim-treesitter.configs").setup(opts)
+    end,
   },
 }
