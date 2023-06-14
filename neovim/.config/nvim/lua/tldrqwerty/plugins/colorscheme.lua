12820d46f4f2ee1919c1b828c587bdafc500a062
Charlie Carolan <charliecarolan1999@gmail.com>
2023-06-14 01:05:04 +0100

    feat(nvim): change colorscheme

--- neovim/.config/nvim/lua/tldrqwerty/plugins/colorscheme.lua
+++ neovim/.config/nvim/lua/tldrqwerty/plugins/colorscheme.lua
@@ -1,44 +1,39 @@
 return {
   {
     "shaunsingh/oxocarbon.nvim",
-    lazy = true
+    config = function()
+      vim.opt.background = "dark" -- set this to dark or light
+      vim.cmd.colorscheme "oxocarbon"
+    end,
   },
   {
     "ellisonleao/gruvbox.nvim",
-    lazy = true
   },
   {
     "rose-pine/neovim",
     name = "rose-pine",
-    lazy = true,
   },
   {
     "jacoborus/tender.vim",
-    lazy = true,
   },
   {
     "Lokaltog/monotone.nvim",
-    lazy = true,
     dependencies = {
       "rktjmp/lush.nvim",
     },
   },
   {
     "Shatur/neovim-ayu",
-    lazy = true,
   },
   {
     "JoosepAlviste/palenightfall.nvim",
-    lazy = true,
   },
   {
     "savq/melange-nvim",
-    lazy = true,
   },
   {
     "jesseleite/nvim-noirbuddy",
     dependencies = { "tjdevries/colorbuddy.nvim" },
-    lazy = true,
   },
   {
     "catppuccin/nvim",
@@ -47,7 +42,7 @@ return {
       require("catppuccin").setup({
         flavor = "mocha",
       })
-      vim.cmd.colorscheme("catppuccin")
+      -- vim.cmd.colorscheme("catppuccin")
     end,
   },
 }
