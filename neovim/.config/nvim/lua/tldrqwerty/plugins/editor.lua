8f8743d5310c985e3d950be3c4911e57bc1ef14f
Charlie Carolan <charliecarolan1999@gmail.com>
2024-07-25 23:15:58 +0100

    feat(nvim): add yazi plugin

--- neovim/.config/nvim/lua/tldrqwerty/plugins/editor.lua
+++ neovim/.config/nvim/lua/tldrqwerty/plugins/editor.lua
@@ -18,6 +18,42 @@ return {
 		},
 	},
 	{
+		"mikavilpas/yazi.nvim",
+		event = "VeryLazy",
+		-- 👇 in this section, choose your own keymappings!
+		keys = {
+			{
+				"<leader>-",
+				function()
+					require("yazi").yazi()
+				end,
+				desc = "Open the file manager",
+			},
+			{
+				-- Open in the current working directory
+				"<leader>_",
+				function()
+					require("yazi").yazi(nil, vim.fn.getcwd())
+				end,
+				desc = "Open the file manager in nvim's working directory",
+			},
+			{
+				'<c-up>',
+				function()
+					-- NOTE: requires a version of yazi that includes
+					-- https://github.com/sxyazi/yazi/pull/1305 from 2024-07-18
+					require('yazi').toggle()
+				end,
+				desc = "Resume the last yazi session",
+			},
+		},
+		---@type YaziConfig
+		opts = {
+			-- if you want to open yazi instead of netrw, see below for more info
+			open_for_directories = false,
+		}
+	},
+	{
 		"kdheepak/lazygit.nvim",
 		cmd = {
 			"LazyGit",
